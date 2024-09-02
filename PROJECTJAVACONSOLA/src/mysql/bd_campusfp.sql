af191119916903dbc54a766ada1a35666bdfb7eb
Walter Ismael Sagástegui Lescano <78830143+DATASAGASTEGUI@users.noreply.github.com>
2024-09-02 11:46:42 +0200

    Update bd_campusfp.sql

--- PROJECTJAVACONSOLA/src/mysql/bd_campusfp.sql
+++ PROJECTJAVACONSOLA/src/mysql/bd_campusfp.sql
@@ -46,4 +46,33 @@ INSERT INTO Alumno (idAlumno, nombre, apellidos, grupo, fechaNacimiento) VALUES 
 
 SELECT * FROM Alumno;
 
+DELIMITER $$
+
+CREATE FUNCTION NombreMes(num_mes INT)
+RETURNS VARCHAR(15)
+DETERMINISTIC
+BEGIN
+    DECLARE nombre VARCHAR(15);
+
+    SET nombre = CASE
+        WHEN num_mes = 1 THEN 'Enero'
+        WHEN num_mes = 2 THEN 'Febrero'
+        WHEN num_mes = 3 THEN 'Marzo'
+        WHEN num_mes = 4 THEN 'Abril'
+        WHEN num_mes = 5 THEN 'Mayo'
+        WHEN num_mes = 6 THEN 'Junio'
+        WHEN num_mes = 7 THEN 'Julio'
+        WHEN num_mes = 8 THEN 'Agosto'
+        WHEN num_mes = 9 THEN 'Septiembre'
+        WHEN num_mes = 10 THEN 'Octubre'
+        WHEN num_mes = 11 THEN 'Noviembre'
+        WHEN num_mes = 12 THEN 'Diciembre'
+        ELSE 'Mes Inválido'
+    END;
+
+    RETURN nombre;
+END$$
+
+DELIMITER ;
+
 
